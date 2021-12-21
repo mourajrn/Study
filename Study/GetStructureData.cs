21304f774baf73e1b380b66d5d7d0b6e0bb5fcec
José de Moura <39865466+mourajrn@users.noreply.github.com>
2021-12-20 23:47:39 -0300

    Primeira versão do GetStructureData

---
+++ Study/GetStructureData.cs
@@ -0,0 +1,30 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Study
+{
+    [Transaction(TransactionMode.Manual)]
+    public class GetStructureData : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            List<Element> beams = new FilteredElementCollector(doc).WhereElementIsNotElementType().OfCategory(BuiltInCategory.OST_StructuralFraming).Where(b => b.LookupParameter("Material estrutural").AsValueString().Contains("Concreto") && !b.LookupParameter("Material estrutural").AsValueString().Contains("Treliçadas")).ToList();
+            List<Element> columns = new FilteredElementCollector(doc).WhereElementIsNotElementType().OfCategory(BuiltInCategory.OST_StructuralColumns).ToList();
+            List<Element> floors = new FilteredElementCollector(doc).WhereElementIsNotElementType().OfCategory(BuiltInCategory.OST_Floors).ToList();
+
+            return Result.Succeeded;
+        }
+    }
+}
